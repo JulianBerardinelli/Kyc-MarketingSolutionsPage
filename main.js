2608f1f000d4749832081ae098c6ee5e43f96b90
Julian Berardinelli <julian.berardinelli@gmail.com>
2023-08-09 09:38:18 -0300

    indexload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const header = document.querySelector("header");
-let lastScrollY = window.scrollY;
+// const header = document.querySelector("header");
+// let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-        header.classList.add("header--hidden");
-    } else {
-        header.classList.remove("header--hidden");
-    }
+// window.addEventListener("scroll", () => {
+//     if (lastScrollY < window.scrollY) {
+//         header.classList.add("header--hidden");
+//     } else {
+//         header.classList.remove("header--hidden");
+//     }
     
-    lastScrollY = window.scrollY;
-});
+//     lastScrollY = window.scrollY;
+// });
